ff3bd6393a10ad1f8c068c41d53a8a7d34fcb658
Greymouth <thejadenjack@gmail.com>
2017-07-29 01:46:43 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
